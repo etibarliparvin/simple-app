cd6b75406f96daa42b06a19cc78f6225074a7573
Parvin Etibarli <etibarliparvin@gmail.com>
2026-02-24 19:30:58 +0400

    fatime1

--- src/main/java/com/appsdeveloperblog/ws/Main.java
+++ src/main/java/com/appsdeveloperblog/ws/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static void fatime1() {
+        System.out.println("fatime1");
+    }
+
 }
